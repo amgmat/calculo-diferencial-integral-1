dc29b68ae757fdc335466bead030289436583f09
Ale <amg_29-90@hotmail.com>
2019-09-05 15:30:27 -0500

    ACTUALIZACION SW

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,5 +1,5 @@
 var url = window.location.href;
-var swLocation = 'calculo-diferencial-integral-I/sw.js';
+var swLocation = '/calculo-diferencial-integral-I/sw.js';
 
 if (navigator.serviceWorker) {
 
