85ab1af1fbc215a2801a2b23d3038684b0d8e724
Ale <amg_29-90@hotmail.com>
2019-09-05 11:01:25 -0500

    docs sw

--- docs/sw.js
+++ docs/sw.js
@@ -1,11 +1,11 @@
-importScripts('sw-utils.js');
+importScripts('js/sw-utils.js');
 
 const STATIC_CACHE = 'static-v3';
 const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'styles.f0080354005001a24de8.css',
     'img/favicon.png',
