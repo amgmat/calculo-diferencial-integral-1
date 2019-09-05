bb1a84d5b0b425dcda9d7e4567f7a3abca5e7a9e
Ale <amg_29-90@hotmail.com>
2019-09-05 11:28:17 -0500

    cambios en inmutable

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v2';
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -15,8 +15,8 @@ const APP_SHELL = [
 ];
 
 const APP_SHELL_INMUTABLE = [
-    'js\libs/plugins/mdtoast.min.css',
-    'js\libs/plugins/mdtoast.min.js'
+    'js/libs/plugins/mdtoast.min.css',
+    'js/libs/plugins/mdtoast.min.js'
 ];
 
 self.addEventListener('install', e => {
