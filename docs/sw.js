5c632ad7f0f6f04ece42d4102ea1a13901608952
Ale <amg_29-90@hotmail.com>
2019-09-05 22:25:17 -0500

    mejoras

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v3';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v4';
 const INMUTABLE_CACHE = 'inmutable-v2';
 
 const APP_SHELL = [
